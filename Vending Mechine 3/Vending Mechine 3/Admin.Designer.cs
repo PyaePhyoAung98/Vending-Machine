155cb0198b17f0b7ac3c9c13e2723c5a3338f5fe
Mg Pyae Phyo Aung <41873340+PyaePhyoAung98@users.noreply.github.com>
2020-04-01 10:55:52 +0630

    Add files via upload

---
+++ Vending Mechine 3/Vending Mechine 3/Admin.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Vending_Mechine_3
+{
+    partial class Admin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.item = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.qty = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.total = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btn_Insert = new System.Windows.Forms.Button();
+            this.btn_Update = new System.Windows.Forms.Button();
+            this.btn_Delete = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.item,
+            this.qty,
+            this.price,
+            this.total});
+            this.dataGridView1.Location = new System.Drawing.Point(78, -1);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(591, 228);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // item
+            // 
+            this.item.HeaderText = "Item";
+            this.item.Name = "item";
+            this.item.ReadOnly = true;
+            // 
+            // qty
+            // 
+            this.qty.HeaderText = "Quantity";
+            this.qty.Name = "qty";
+            this.qty.ReadOnly = true;
+            // 
+            // price
+            // 
+            this.price.HeaderText = "Price";
+            this.price.Name = "price";
+            this.price.ReadOnly = true;
+            // 
+            // total
+            // 
+            this.total.HeaderText = "Total";
+            this.total.Name = "total";
+            this.total.ReadOnly = true;
+            // 
+            // btn_Insert
+            // 
+            this.btn_Insert.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Insert.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Insert.ForeColor = System.Drawing.Color.Red;
+            this.btn_Insert.Location = new System.Drawing.Point(98, 259);
+            this.btn_Insert.Name = "btn_Insert";
+            this.btn_Insert.Size = new System.Drawing.Size(139, 34);
+            this.btn_Insert.TabIndex = 1;
+            this.btn_Insert.Text = "Insert Item";
+            this.btn_Insert.UseVisualStyleBackColor = false;
+            this.btn_Insert.Click += new System.EventHandler(this.btn_Insert_Click);
+            // 
+            // btn_Update
+            // 
+            this.btn_Update.BackColor = System.Drawing.Color.Black;
+            this.btn_Update.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Update.ForeColor = System.Drawing.Color.Red;
+            this.btn_Update.Location = new System.Drawing.Point(308, 259);
+            this.btn_Update.Name = "btn_Update";
+            this.btn_Update.Size = new System.Drawing.Size(136, 34);
+            this.btn_Update.TabIndex = 2;
+            this.btn_Update.Text = "Update Item";
+            this.btn_Update.UseVisualStyleBackColor = false;
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.BackColor = System.Drawing.Color.Black;
+            this.btn_Delete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Delete.ForeColor = System.Drawing.Color.Red;
+            this.btn_Delete.Location = new System.Drawing.Point(518, 259);
+            this.btn_Delete.Name = "btn_Delete";
+            this.btn_Delete.Size = new System.Drawing.Size(131, 34);
+            this.btn_Delete.TabIndex = 3;
+            this.btn_Delete.Text = "Delete Item";
+            this.btn_Delete.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Black;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.Red;
+            this.button4.Location = new System.Drawing.Point(340, 335);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 33);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Back";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Admin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 462);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btn_Delete);
+            this.Controls.Add(this.btn_Update);
+            this.Controls.Add(this.btn_Insert);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Admin";
+            this.Text = "Admin";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn item;
+        private System.Windows.Forms.DataGridViewTextBoxColumn qty;
+        private System.Windows.Forms.DataGridViewTextBoxColumn price;
+        private System.Windows.Forms.DataGridViewTextBoxColumn total;
+        private System.Windows.Forms.Button btn_Insert;
+        private System.Windows.Forms.Button btn_Update;
+        private System.Windows.Forms.Button btn_Delete;
+        private System.Windows.Forms.Button button4;
+    }
+}
---
+++ Vending Mechine 3/Vending Mechine 3/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vending_Mechine_3
+{
+    public partial class Form1 : Form
+    {
+        private Boolean cocaButtonWasClicked = false;
+        private Boolean pepsiButtonWasClicked = false;
+        private Boolean monsterButtonWasClicked = false;
+        public Form1()
+        {
+            InitializeComponent();
+            cocaSlide.Visible = false;
+            pepsiSlide.Visible = false;
+            monsterSlide.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Sound sys
+            System.Media.SoundPlayer s = new System.Media.SoundPlayer();
+            s.SoundLocation = "C:\\Users\\Acer\\Desktop\\New\\slow.wav";
+            s.Play();
+
+            //priceChecking 
+
+            int cocaPrice = int.Parse(cocaprice.Text);
+            int pepsiPrice = int.Parse(pepsiprice.Text);
+            int monsterPrice = int.Parse(monsterprice.Text);
+
+            int textBox = int.Parse(textBox1.Text);
+
+            string price = textBox1.Text;
+
+            //cocaPrice check and output
+
+            if (cocaButtonWasClicked == true)
+            {
+                if (textBox >= cocaPrice)
+                {
+                    int aa = textBox - cocaPrice;
+                    textBox1.Text = aa.ToString();
+                    count = aa;
+                    cocaSlide.Visible = true;
+                    pepsiSlide.Visible = false;
+                    monsterSlide.Visible = false;
+                }
+
+
+            }
+
+            //pepsiPrice Check and output
+
+            if (pepsiButtonWasClicked == true)
+            {
+
+                if (textBox >= pepsiPrice)
+                {
+                    int aa = textBox - pepsiPrice;
+                    textBox1.Text = aa.ToString();
+                    count = aa;
+                    pepsiSlide.Visible = true;
+                    monsterSlide.Visible = false;
+                    cocaSlide.Visible = false;
+                }
+
+
+            }
+
+            //monsterPriceCheck&Output
+            if (monsterButtonWasClicked == true)
+            {
+
+
+                if (textBox >= monsterPrice)
+                {
+                    int aa = textBox - monsterPrice;
+                    textBox1.Text = aa.ToString();
+                    count = aa;
+                    monsterSlide.Visible = true;
+                    pepsiSlide.Visible = false;
+                    cocaSlide.Visible = false;
+                }
+
+            }
+
+
+        }
+
+        private void coca1_Click(object sender, EventArgs e)
+        {
+
+
+            button1.Enabled = true;
+            cocaButtonWasClicked = true;
+
+
+
+        }
+
+        private void coca2_Click(object sender, EventArgs e)
+        {
+
+            button1.Enabled = true;
+            cocaButtonWasClicked = true;
+
+
+        }
+
+        private void coca3_Click(object sender, EventArgs e)
+        {
+
+            button1.Enabled = true;
+            cocaButtonWasClicked = true;
+
+        }
+
+        private void pepsi1_Click(object sender, EventArgs e)
+        {
+            int textBox = int.Parse(textBox1.Text);
+
+            button1.Enabled = true;
+            pepsiButtonWasClicked = true;
+            if (textBox == 0 || textBox < 10)
+            {
+                button1.Enabled = false;
+            }
+        }
+
+        private void pepsi2_Click(object sender, EventArgs e)
+        {
+            int textBox = int.Parse(textBox1.Text);
+            button1.Enabled = true;
+            pepsiButtonWasClicked = true;
+            if (textBox == 0 || textBox < 10)
+            {
+                button1.Enabled = false;
+            }
+
+        }
+
+        private void pepsi3_Click(object sender, EventArgs e)
+        {
+            int textBox = int.Parse(textBox1.Text);
+            button1.Enabled = true;
+            pepsiButtonWasClicked = true;
+            if (textBox == 0 || textBox < 10)
+            {
+                button1.Enabled = false;
+            }
+        }
+
+        private void monster1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            monsterButtonWasClicked = true;
+        }
+
+        private void monster2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            monsterButtonWasClicked = true;
+        }
+
+        private void monster3_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            monsterButtonWasClicked = true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int cocaPrice = int.Parse(cocaprice.Text);
+            int pepsiPrice = int.Parse(pepsiprice.Text);
+            int monsterPrice = int.Parse(monsterprice.Text);
+            int textBox = int.Parse(textBox1.Text);
+
+            if (textBox == 5)
+            {
+                textBox1.Text = "0" + textBox;
+            }
+            if (textBox >= cocaPrice)
+            {
+                coca1.Enabled = true;
+                coca2.Enabled = true;
+                coca3.Enabled = true;
+
+            }
+            if (textBox >= pepsiPrice)
+            {
+                pepsi1.Enabled = true;
+                pepsi2.Enabled = true;
+                pepsi3.Enabled = true;
+            }
+            if (textBox >= monsterPrice)
+            {
+                monster1.Enabled = true;
+                monster2.Enabled = true;
+                monster3.Enabled = true;
+            }
+        }
+        public int count;
+
+        private void btn_5_Click(object sender, EventArgs e)
+        {
+            count += 5;
+            textBox1.Text = count.ToString();
+        }
+
+        private void btn_10_Click(object sender, EventArgs e)
+        {
+            count += 10;
+            textBox1.Text = count.ToString();
+        }
+
+        private void cocaSlide_Click(object sender, EventArgs e)
+        {
+            cocaSlide.Visible = false;
+            button1.Enabled = false;
+        }
+
+        private void pepsiSlide_Click(object sender, EventArgs e)
+        {
+            pepsiSlide.Visible = false;
+
+            button1.Enabled = false;
+        }
+
+        private void monsterSlide_Click(object sender, EventArgs e)
+        {
+            monsterSlide.Visible = false;
+
+            button1.Enabled = false;
+        }
+
+        private void admin_Sitting_Click(object sender, EventArgs e)
+        {
+            Admin admin = new Admin();
+            admin.Show();
+            Form1 mainform = new Form1();
+            mainform.Close();
+            
+        }
+    }
+}
